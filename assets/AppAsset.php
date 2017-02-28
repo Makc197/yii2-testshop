da7af4ad7ae80759e2b27c99118ed9993cdab576
MAKC <maxim.s.lazarev@gmail.com>
2017-02-28 18:44:23 +0300

    Add New Template to YII2

---
+++ assets/AppAsset.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle {
+
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'theme/css/site.css',
+        'theme/css/bootstrap.min.css',
+        'theme/css/icomoon-social.css',
+        'theme/css/leaflet.css',
+        'theme/css/main.css'
+    ];
+    public $js = [
+        'theme/js/bootstrap.min.js',
+        'theme/js/jquery.fitvids.js',
+        'theme/js/jquery.sequence-min.js',
+        'theme/js/jquery.bxslider.js',
+        'theme/js/main-menu.js',
+        'theme/js/template.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+
+}
---
+++ views/layouts/footer.php
@@ -0,0 +1,48 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-footer col-md-3 col-xs-6" style="height: 235px;">
+                <h3>Our Latest Work</h3>
+                <div class="portfolio-item">
+                    <div class="portfolio-image">
+                        <a href="theme/page-portfolio-item.html"><img src="theme/img/portfolio6.jpg" alt="Project Name"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-footer col-md-3 col-xs-6" style="height: 235px;">
+                <h3>Navigate</h3>
+                <ul class="no-list-style footer-navigate-section">
+                    <li><a href="theme/page-blog-posts.html">Blog</a></li>
+                    <li><a href="theme/page-portfolio-3-columns-2.html">Portfolio</a></li>
+                    <li><a href="theme/page-products-3-columns.html">eShop</a></li>
+                    <li><a href="theme/page-services-3-columns.html">Services</a></li>
+                    <li><a href="theme/page-pricing.html">Pricing</a></li>
+                    <li><a href="theme/page-faq.html">FAQ</a></li>
+                </ul>
+            </div>
+
+            <div class="col-footer col-md-4 col-xs-6">
+                <h3>Contacts</h3>
+                <p class="contact-us-details">
+                    <b>Address:</b> 123 Fake Street, LN1 2ST, London, United Kingdom<br>
+                    <b>Phone:</b> +44 123 654321<br>
+                    <b>Fax:</b> +44 123 654321<br>
+                    <b>Email:</b> <a href="mailto:getintoutch@yourcompanydomain.com">getintoutch@yourcompanydomain.com</a>
+                </p>
+            </div>
+            <div class="col-footer col-md-2 col-xs-6">
+                <h3>Stay Connected</h3>
+                <ul class="footer-stay-connected no-list-style">
+                    <li><a href="#" class="facebook"></a></li>
+                    <li><a href="#" class="twitter"></a></li>
+                    <li><a href="#" class="googleplus"></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer-copyright">© 2013 mPurpose. All rights reserved.</div>
+            </div>
+        </div>
+    </div>
+</div>
